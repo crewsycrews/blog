794aac39f614fe134293519c15454d7efe0e9d2c
CrewsyCrews <danre1@bk.ru>
2021-02-10 19:25:05 +0300

    Initial commit

---
+++ src/components/Bio.svelte
@@ -0,0 +1,28 @@
+<style>
+  div {
+    align-items: center;
+    display: flex;
+  }
+
+  img {
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+    /* border: 3px solid #fd6378; */
+    display: block;
+    margin-right: 20px;
+  }
+
+  p {
+    font-size: 1.125rem;
+  }
+</style>
+
+<div>
+  <img src="profile-pic.png" alt="Maxi Ferreira">
+  <p>
+    Hi, I'm <strong>Danil</strong>. I'm a software engineer from somewhere in Russia(traveler).
+    You can see some of my work on <a href="https://www.github.com/crewsycrews" target="_blank">GitHub</a>,
+    or get some useful links about me on <a href="https://crewsycrews.github.io/crewsycrews.prod/" target="_blank">my website</a>.
+  </p>
+</div>
